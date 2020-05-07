c4f3654f7b32585433e5887b752efa32b8d876a0
Adam Foerster <adam.foerster@gmail.com>
2020-05-07 17:09:08 -0300

    build complete

--- build-n-inject.js
+++ build-n-inject.js
@@ -1,39 +1,62 @@
-const exec = require('child_process').exec;
-
+const child_process = require("child_process");
+const fs = require("fs");
 const state = {
   branch: null,
-}
+  config: null,
+};
 
-const buildCommand = (error, stdout, stderr) => {
-  console.log(stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-    throw(error);
-  }
-  console.log(stdout);
-  getBranch(state)
-  injectFiles()
-}
+const buildNInject = async (state) => {
+  await loadConfig(state);
+  getBranch(state);
+  build(state);
+  copyDir(state);
+  console.log(state);
+  injectFiles();
+};
 
-const getBranch = state => {
-  exec('git status', (error, stdout, stderr) => {
-    if (error !== null) {
-      console.log(error)
-      throw(new Error(error))
+const getBranch = async (state) => {
+  child_process.execSync("git status > .bni-status");
+  const stdout = fs.readFileSync(".bni-status", "utf8");
+  child_process.exec("rm .bni-status");
+  stdout
+    .split("\n")
+    .forEach((line) =>
+      line.search("On branch") === 0 ? (state.branch = line.substr(10)) : null
+    );
+};
+
+const build = (state) => {
+  child_process.execSync(
+    "cd investimentos && " + state.config.commands[state.branch]
+  );
+};
+
+const copyDir = (state) => {
+  state.config.environments.forEach((env) => {
+    if (env.branch === state.branch) {
+      if (process.platform === "win32") {
+        child_process.execSync(`if exist ${env.copyDir} rmdir ${env.copyDir} /s /q`);
+        child_process.execSync(`mkdir ${env.copyDir}`);
+        child_process.execSync(
+          `xcopy .\\${state.config.buildDir.replace("/","\\")} .\\${env.copyDir} /e`
+        );
+      } else {
+        child_process.execSync(`rm -rf ${env.copyDir}`);
+        child_process.execSync(`mkdir ${env.copyDir}`);
+        child_process.execSync(
+          `cp -R ./${state.config.buildDir} ${env.copyDir}/`
+        );
+      }
     }
-    stdout
-      .split("\n")
-      .forEach(line => line.search('On branch') === 0 ? state.branch = line.substr(10) : null)
-    if (!state.branch) {
-      throw( new Error('Branch not found!'))
-    }
-  })
-}
+  });
+};
 
-const injectFiles = () => {
-  
-}
+const injectFiles = () => {};
 
-const child = exec('npm install sass', buildCommand);
+const loadConfig = (state) => {
+  state.config = JSON.parse(
+    fs.readFileSync("build-n-inject.config.json", "utf8")
+  );
+};
 
-
+buildNInject(state);
