314b8fa2056ecf8d425739adaf3439d7e9c50a03
Adam Foerster <adam.foerster@gmail.com>
2020-05-13 14:20:40 -0300

    ewgular functions

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ const state = {
   config: null,
 };
 
-const main = (state) => {
+function main(state) {
+  console.log("Loading config file");
   loadConfig(state);
+  console.log("Asserting branch");
   getBranch(state);
+  console.log("Building");
   build(state);
-};
+  console.log("all done");
+}
 
-const loadConfig = (state) => {
+function loadConfig(state) {
   if (!fs.existsSync("./build-n-inject.config.json")) {
     throw "build-n-inject.config.json file not found!";
   }
   state.config = JSON.parse(
     fs.readFileSync("build-n-inject.config.json", "utf8")
   );
-};
+}
 
-const getBranch = async (state) => {
+function getBranch(state) {
   child_process.execSync("git status > .bni-status");
   const stdout = fs.readFileSync(".bni-status", "utf8");
   child_process.exec("rm .bni-status");
@@ -29,38 +33,28 @@ const getBranch = async (state) => {
     .forEach((line) =>
       line.search("On branch") === 0 ? (state.branch = line.substr(10)) : null
     );
-};
+}
 
-const build = (state) => {
+function build(state) {
   state.config.environments.forEach((env) => {
     if (env.branch === state.branch) {
       child_process.execSync(`rm -rf ${env.copyDir}`);
       if (env.fileReplacements) {
         env.fileReplacements.forEach(([orig, dest]) => {
-          child_process.execSync(
-            `mv ${orig} ${orig}.bkp`
-          );
-          child_process.execSync(
-            `cp ${dest} ${orig}`
-          );
+          child_process.execSync(`mv ${orig} ${orig}.bkp`);
+          child_process.execSync(`cp ${dest} ${orig}`);
         });
       }
       child_process.execSync(env.build);
       if (env.fileReplacements) {
         env.fileReplacements.forEach(([orig, dest]) => {
-          child_process.execSync(
-            `mv ${orig} ${dest}`
-          );
-          child_process.execSync(
-            `mv ${orig}.bkp ${orig}`
-          );
+          child_process.execSync(`mv ${orig} ${dest}`);
+          child_process.execSync(`mv ${orig}.bkp ${orig}`);
         });
       }
-      child_process.execSync(
-        `mv ./${state.config.buildDir} ./${env.copyDir}`
-      );
+      child_process.execSync(`mv ./${state.config.buildDir} ./${env.copyDir}`);
     }
   });
-};
+}
 
 main(state);
